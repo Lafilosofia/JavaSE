ec50a21992f5bca417c36c62a2e56b50ffcec360
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2021-01-04 14:17:43 +0800

    Add files via upload

---
+++ day04/PrintWriterDemo.java
@@ -0,0 +1,14 @@
+package day04;
+/**
+ * 缓冲字符流
+ * java.io.BufferedWriter
+ * java.in.BufferedReader
+ * 
+ * java.io.PrintWrite
+ * 带有自动行刷新的缓冲字符输出流（经常用）
+ * @author 86180
+ *
+ */
+public class PrintWriterDemo {
+
+}
