049237654b5ac03a9f45bfadd6bb2c5017819248
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2021-01-07 14:16:57 +0800

    Add files via upload

---
+++ day05/NameAlreadyException.java
@@ -0,0 +1,33 @@
+package day05;
+
+public class NameAlreadyException extends RuntimeException {
+
+	public NameAlreadyException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameAlreadyException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameAlreadyException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameAlreadyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NameAlreadyException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
